d1b6fea03d2638bcf458e305cf645866ed0c591f
troybowman <troybowman2000@gmail.com>
2018-11-06 07:24:50 -0800

    added build script

---
+++ build.py
@@ -0,0 +1,54 @@
+import sys
+import subprocess
+import shutil
+import os
+import glob
+import re
+
+QTDIR = "/Users/Shared/Qt/5.6.3-x64/"
+
+def patch_qt_install_names(lib):
+    rpath_re = re.compile("(@rpath/Qt[^\s]+\.framework/Versions/[0-9]+/Qt[^\s]+).*")
+    for line in subprocess.check_output(["otool", "-L", lib]).split("\n\t"):
+        match = rpath_re.match(line)
+        if match:
+            orig = match.group(1)
+            if not orig.endswith("_debug"):
+                dest = orig + "_debug"
+                argv = [ "install_name_tool", "-change", orig, dest, lib ]
+                print "patching install name: %s" % argv
+                subprocess.check_call(argv)
+
+# We use this to point the install names to debug Qt frameworks, because of https://bugreports.qt.io/browse/QTBUG-48800.
+def make_macdeployqt_actually_work(qtdir, appdir, pi):
+    # patch pi to refer to debug Qt Libs
+    patch_qt_install_names(os.path.join(appdir, "Contents", "MacOS", pi))
+    # patch the debug Qt libs themselves
+    for lib in glob.glob(os.path.join(appdir, "Contents", "Frameworks", "Qt*.framework", "Versions", "Current", "*_debug")):
+        patch_qt_install_names(lib)
+    # use libqcocoa_debug.dylib instead of libqcocoa.dylib, and patch it as well
+    libqcocoa = os.path.join(appdir, "Contents", "PlugIns", "platforms", "libqcocoa%s.dylib")
+    if os.path.isfile(libqcocoa % ""):
+        os.remove(libqcocoa % "")
+        shutil.copy(os.path.join(qtdir, "plugins", "platforms", "libqcocoa_debug.dylib"), os.path.dirname(libqcocoa))
+        patch_qt_install_names(libqcocoa % "_debug")
+
+# start over
+try:
+    os.remove(".qmake.stash")
+    os.remove("Makefile")
+    shutil.rmtree("obj")
+    shutil.rmtree("pi.app")
+except OSError:
+    pass
+
+# build pi.app
+subprocess.check_call([QTDIR + "bin/qmake"])
+subprocess.check_call(["make"])
+
+# create a standalone bundle
+subprocess.check_call([QTDIR + "bin/macdeployqt", "pi.app", "-use-debug-libs"])
+# QtNetwork is needed for squish tests
+shutil.copytree(QTDIR + "lib/QtNetwork.framework", "pi.app/Contents/Frameworks/QtNetwork.framework", symlinks=True)
+# Qt sucks at developing applications
+make_macdeployqt_actually_work(QTDIR, "pi.app", "pi")
